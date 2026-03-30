736bce89ddda1597b22e42224da9469a8a42e93e
Julya Vitoria Oliveira Garcia <julyagarcia332@gmail.com>
2026-03-29 22:32:35 -0300

    Commit inicial do projeto

---
+++ app/src/main/java/com/example/cadastrousuario/MainActivity.java
@@ -0,0 +1,65 @@
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    // Declaração de componentes visuais e do adaptador da lista
+
+    RecyclerView recyclerView;
+
+    UserAdapter adapter;
+
+    Button btnCadastrar;
+
+    // Atenção: atributo estático (static) permite que os dados persistam na memória
+    // Enquanto o App estiver aberto e sejam acessados diretamente por outras telas (Activities)
+
+    public static List<String> listaNomes = new ArrayList<>();
+
+    // Método de inicialização da Activity (ponto de entrada da tela)
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        // Vincula o arquivo de layout XML (activity_main.xml) a essa classe java
+        setContentView(R.layout.activity_main);
+
+        // Regra de negócio: Insere um texto no topo da lista, caso ela esteja vazia
+        if(listaNomes.isEmpty()) {
+            listaNomes.add("Nomes de Cadastro"); }
+
+        // Mapeamento dos componentes do RecyclerView do XML para o objeto Java
+        recyclerView = findViewById(R.id.recycler_view);
+
+        // Define o LayoutManager: organiza itens da lista em uma coluna vertical simples
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //Instancia o adaptador passando a nossa lista de nomes
+        adapter = new UserAdapter(listaNomes);
+
+        // Conecta o adaptador ao RecyclerView para que os dados sejam desenhados na tela
+        recyclerView.setAdapter(adapter);
+
+        // Mapeia o botão de cadastro
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+
+        // Configura o evento de clique usando a expressão lambda (java 8+)
+        btnCadastrar.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, CreateUser.class));
+        });
+
+    }
+
+    // Método chamado sempre que a tela volta a ficar visível para o usuário
+    @Override
+    protected void onResume(){
+        super.onResume();
+        // Avisar o adaptador que a fonte de dados (listaNomes) pode ter sido alterada na outra tela, forçando a atualização da lista
+        adapter.notifyDataSetChanged();
+    }
+}
